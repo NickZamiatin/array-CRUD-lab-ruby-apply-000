cc3e2afba6053a846b8b05738927532a7411272f
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-12-01 17:24:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["hello","my","name" "is"]
-  element.push= ("Nick")
+  element.push= ('Nick')
 end
 
 def add_element_to_start_of_array(array, element)
